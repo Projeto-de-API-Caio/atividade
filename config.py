7092492fc9b74d721ddd14cc80fcba85a1b47e73
Filipe Rocha <felipe.kw@hotmail.com>
2025-05-28 19:17:08 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///atividade.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['DEBUG'] = True
+
+    db.init_app(app)
+
+    return app
